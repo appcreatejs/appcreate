1b2d65cac00579994aff8b430d437702e7f10e08
Gilberto Albino <contato@gilbertoalbino.com>
2019-09-07 20:52:29 -0300

    Modifica entry point index.js

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,4 @@
 #!/usr/bin/env node
 'use strict';
 
-/**
- *
- * AppCreate CLI
- *
- * AppCreate é um pacote para criação de projetos.
- * Este arquivo é o ponto de entrada para o CLI
- * utilizado para criar projetos e executar
- * as diversas funcionalidades existentes.
- *
-*/
-const AppCreate = require('../src/AppCreate');
-
-AppCreate.fromProcess();
+require('../index');
