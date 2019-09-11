6989270b805f5599e1d21bc64086fecece33b86c
Gilberto Albino <galbino@studiovisual.com.br>
2019-09-11 18:54:39 -0300

    Commit demo

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
  * Este arquivo é o ponto de entrada para o CLI
  * utilizado para criar projetos e executar
  * as diversas funcionalidades existentes.
+ * 
+ * fim
  *
 */
 const AppCreate = require('./src/AppCreate');
